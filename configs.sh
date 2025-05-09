de022e8eebb145d326db9f1a4d193f1939cb717a
w88 <krippach147@gmail.com>
2025-05-09 21:44:29 +0500

    Fixed problem with removing old directories

--- configs.sh
+++ configs.sh
@@ -1,32 +1,32 @@
-if [ -d "/home/$USER/.config/fastfetch"]; then
+if [ -d "/home/$USER/.config/fastfetch" ]; then
   rm -R /home/$USER/.config/fastfetch
 fi
 
-if [ -d "/home/$USER/.config/firefox"]; then
+if [ -d "/home/$USER/.config/firefox" ]; then
   rm -R /home/$USER/.config/firefox
 fi
 
-if [-d "/home/$USER/.config/hypr"]; then
+if [ -d "/home/$USER/.config/hypr" ]; then
   rm -R /home/$USER/.config/hypr
 fi
 
-if [-d "/home/$USER/.config/kitty"]; then
+if [ -d "/home/$USER/.config/kitty" ]; then
   rm -R /home/$USER/.config/kitty
 fi
 
-if [-d "/home/$USER/.config/mako"]; then
+if [ -d "/home/$USER/.config/mako" ]; then
   rm -R /home/$USER/.config/mako
 fi
 
-if [-d "/home/$USER/.config/nvim"]; then
+if [ -d "/home/$USER/.config/nvim" ]; then
   sudo rm -R /home/$USER/.config/nvim
 fi
 
-if [-d "/home/$USER/.config/nvim"]; then
+if [ -d "/home/$USER/.config/nvim" ]; then
   sudo rm -R /home/$USER/.config/waybar
 fi
 
-if [-d "/home/$USER/.config/wofi"]; then
+if [ -d "/home/$USER/.config/wofi" ]; then
   rm -R /home/$USER/.config/wofi
 fi
 
