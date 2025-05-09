35481d772402ab10700196191f9c78c7fb81a666
w88 <krippach147@gmail.com>
2025-05-09 19:46:56 +0500

    Added installation of the "Yay"

--- installer.sh
+++ installer.sh
@@ -1,6 +1,20 @@
 echo "Starting installing process."
 sudo pacman -Syu --noconfirm
 sudo pacman -S --needed - < pkglist.txt
+
+if ! command -v yay &> /dev/null; then
+  echo "Installing 'Yay'..."
+  tmpdir=$(mktemp -d)
+  git clone https://aur.archlinux.org/yay.git "$tmpdir"
+  pushd "$tmpdir" || exit 1
+  makepkg -si --noconfirm
+  popd
+  rm -rf "$tmpdir"
+  echo "'Yay' is installed."
+else
+  echp "'Yay' has already installed."
+fi
+
 python -m venv instenv
 source instenv/bin/activate
 pip install art
