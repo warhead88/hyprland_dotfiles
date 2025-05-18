c58c013ad71c9d06b393c5e0fc6f199cc50a43bf
w88 <krippach147@gmail.com>
2025-05-18 12:28:43 +0500

    Fixed problem with firefox configs

--- dotfiles/firefox/installer.sh
+++ dotfiles/firefox/installer.sh
@@ -8,8 +8,8 @@ dir="$(cd "$(dirname "$0")" && pwd)"
 target_dir=$dir/dotfiles/firefox
 
 mkdir -p /home/$USER/.config/firefox
-cp -R icons /home/$USER/.config/firefox/icons
-cp index.html /home/$USER/.config/firefox/index.html
-cp private.html /home/$USER/.config/firefox/private.html
-cp style.css /home/$USER/.config/firefox/style.css
-cp style_private.css /home/$USER/.config/firefox/style_private.css
+cp -R $target_dir/icons /home/$USER/.config/firefox/icons
+cp $target_dir/index.html /home/$USER/.config/firefox/index.html
+cp $target_dir/private.html /home/$USER/.config/firefox/private.html
+cp $target_dir/style.css /home/$USER/.config/firefox/style.css
+cp $target_dir/style_private.css /home/$USER/.config/firefox/style_private.css
